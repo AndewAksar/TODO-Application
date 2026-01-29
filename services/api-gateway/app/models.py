4d8160d7015263c9993829b394062a9a150a5b61
Andrew A. <aandrew773@gmail.com>
2026-01-30 00:50:07 +0500

    fix(infra): make alembic work with postgres inside docker

--- services/api-gateway/app/models.py
+++ services/api-gateway/app/models.py
@@ -1,9 +1,9 @@
 from app.db import Base
-from sqlalchemy import String
+from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(String, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
--- services/api-gateway/app/settings.py
+++ services/api-gateway/app/settings.py
@@ -1,8 +1,16 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+# parents[3] -> TODO-Application (корень репо)
+
+
+ENV_FILE = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file=ENV_FILE, extra="ignore")
 
     DATABASE_URL: str
 
